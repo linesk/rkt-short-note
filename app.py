408fe22286b1137086c81bbd8e0bd2cd1fdfdeea
Ratchapoom <Ratchapoom.k@kkumail.com>
2019-01-07 20:59:16 +0700

    sdk

--- app.py
+++ app.py
@@ -1,21 +1,59 @@
 import os
 import sys
-from argparse import ArgumentParser
+from flask import Flask, request, abort
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+)
 
 from flask import Flask, request, abort
 
 app = Flask(__name__)
 
 
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 
-@app.route("/webhook", methods=['POST','GET'])
-def webhook():
-    if request.method == 'POST':
-        return 'OK'
+@app.route("/webhook", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    line_bot_api.reply_message(event.reply_token,
+                               TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
