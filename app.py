96c5cb14f1eec96c906ce734b4dcdcd593c20829
Ratchapoom <Ratchapoom.k@kkumail.com>
2019-01-07 20:50:56 +0700

    line sdk

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+import os
+import sys
+from argparse import ArgumentParser
+
+from flask import Flask, request, abort
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/webhook", methods=['POST'])
+def webhook(request):
+    if request.method == 'POST':
+        return 'OK'
+
 
 if __name__ == "__main__":
     app.run()
