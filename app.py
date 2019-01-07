d067f190571547c470e77e3f711aa17ef818fa92
Ratchapoom <Ratchapoom.k@kkumail.com>
2019-01-07 21:55:17 +0700

    her

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-
+    app.logger.info("Request body: " + body)
     # handle webhook body
     try:
         handler.handle(body, signature)
